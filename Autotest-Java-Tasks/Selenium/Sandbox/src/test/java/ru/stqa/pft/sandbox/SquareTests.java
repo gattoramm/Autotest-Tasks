2222a6749cf584ed2409eb3145342171a3afa101
RAMIL ARSAEV <gattoramm@gmail.com>
2021-09-12 20:16:09 +0300

    nn

--- Autotest-Java-Tasks/Selenium/Sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ Autotest-Java-Tasks/Selenium/Sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -1,11 +1,12 @@
 package ru.stqa.pft.sandbox;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SquareTests {
     @Test
     public void testArea() {
         Square square = new Square(5);
-        assert square.area() == 25;
+        Assert.assertEquals(square.area(),25.0);
     }
 }
